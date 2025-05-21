e5d0c90d280f100b6b2c6fa312eb36f6cbfcb20c
NTT-97 <56031563+NTT-97@users.noreply.github.com>
2025-05-21 15:52:55 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  base: '/<REPO_NAME>/', // Thay <REPO_NAME> bằng tên repo GitHub của bạn
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    base: '/SummerCamp2025_MW/', // 👈 đường dẫn đúng để dùng với GitHub Pages
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+  };
 });
-
